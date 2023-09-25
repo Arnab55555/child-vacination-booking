d493e12070f7821dd2ecc1273ab49e6c7f1e18e0
Arnab55555 <arnabsamanta555@gmail.com>
2023-09-25 21:45:08 +0530

    changes

--- web.py
+++ web.py
@@ -29,35 +29,9 @@ def services():
 
 @web.route("/Vacination Schedule/",methods=["GET","POST"])
 def schedule():
-    email_reciever = request.form["em"]
-    try:
-        
-        email_sender = 'autolibpy@gmail.com'
-        email_password = 'epemdruoebcgmtta'
-        
-        print(email_reciever)
-
-        subject = 'Booking Confirmation'
-        body = """
-            Welcome to out vacination center
-            """
-    
-        em = EmailMessage()
-        em['From'] = email_sender
-        em['To'] = email_reciever
-        em['subject'] = subject
-        em.set_content(body)
-
-        context = ssl.create_default_context()
-
-        with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
-            smtp.login(email_sender, email_password)
-            smtp.sendmail(email_sender, email_reciever, em.as_string())
-
-    except:
-        print(email_reciever)
-        print("incorrect automation")
-
+    print("Hello")
+    patientName = request.form['patientname']
+    print(patientName)
     
 
     return render_template("schedule.html")
@@ -81,3 +55,38 @@ def contactUs():
 
 if __name__=="__main__":
     web.run(debug=True,port=7000)
+
+
+
+
+
+
+
+# email_reciever = request.form["em"]
+#     try:
+        
+#         email_sender = 'autolibpy@gmail.com'
+#         email_password = 'epemdruoebcgmtta'
+        
+#         print(email_reciever)
+
+#         subject = 'Booking Confirmation'
+#         body = """
+#             Welcome to out vacination center
+#             """
+    
+#         em = EmailMessage()
+#         em['From'] = email_sender
+#         em['To'] = email_reciever
+#         em['subject'] = subject
+#         em.set_content(body)
+
+#         context = ssl.create_default_context()
+
+#         with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+#             smtp.login(email_sender, email_password)
+#             smtp.sendmail(email_sender, email_reciever, em.as_string())
+
+#     except:
+#         print(email_reciever)
+#         print("incorrect automation")
