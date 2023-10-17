0e5c18c307da4870442b8ee09935e7c926bb99b0
Arnab55555 <arnabsamanta555@gmail.com>
2023-10-17 12:51:22 +0530

    changes

--- services.js
+++ services.js
@@ -1,38 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // BMI Calculator
+document.addEventListener("DOMContentLoaded", function() {
+    const calculateBtn = document.getElementById("calculate-bmi-btn");
     const heightInput = document.getElementById("height");
     const weightInput = document.getElementById("weight");
-    const calculateBMIBtn = document.getElementById("calculate-bmi-btn");
     const bmiValue = document.getElementById("bmi-value");
     const bmiCategory = document.getElementById("bmi-category");
-
-    calculateBMIBtn.addEventListener("click", calculateBMI);
-
-    function calculateBMI() {
-        const height = parseFloat(heightInput.value);
-        const weight = parseFloat(weightInput.value);
-
-        if (!isNaN(height) && !isNaN(weight) && height > 0 && weight > 0) {
-            const bmi = weight / ((height / 100) * (height / 100));
-            bmiValue.textContent = bmi.toFixed(2);
-
-            if (bmi < 18.5) {
-                bmiCategory.textContent = "Underweight";
-                bmiCategory.style.color = "#FF9800";
-            } else if (bmi < 24.9) {
-                bmiCategory.textContent = "Normal weight";
-                bmiCategory.style.color = "#4CAF50";
-            } else if (bmi < 29.9) {
-                bmiCategory.textContent = "Overweight";
-                bmiCategory.style.color = "#FFC107";
-            } else {
-                bmiCategory.textContent = "Obese";
-                bmiCategory.style.color = "#F44336";
-            }
+  
+    calculateBtn.addEventListener("click", function() {
+      const height = parseFloat(heightInput.value);
+      const weight = parseFloat(weightInput.value);
+  
+      if (!isNaN(height) && !isNaN(weight) && height > 0 && weight > 0) {
+        // Calculate BMI
+        const bmi = (weight / ((height / 100) ** 2)).toFixed(2);
+        console.log(bmi)
+  
+        // Display the result
+        bmiValue.textContent = bmi;
+  
+        // Determine BMI category
+        if (bmi < 18.5) {
+          bmiCategory.textContent = "Underweight";
+          bmiCategory.style.color = "#FF9800";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+          bmiCategory.textContent = "Normal Weight";
+          bmiCategory.style.color = "#4CAF50";
+        } else if (bmi >= 25 && bmi < 29.9) {
+          bmiCategory.textContent = "Overweight";
+          bmiCategory.style.color = "#FFC107";
         } else {
-            alert("Please enter valid height and weight.");
+          bmiCategory.textContent = "Obese";
+          bmiCategory.style.color = "#F44336";
         }
-    }
-
-    // Additional JavaScript for other features can be added here
+      } else {
+        alert("Please enter valid height and weight values.");
+      }
+    });
 });
